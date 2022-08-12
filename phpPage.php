70082d1f28bc9f67cbdc2ea0347b9f6e19703bbb
justforawork <justforawork8@gmail.com>
2022-08-12 12:49:43 +0300

    example v1.0

---
+++ phpPage.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+	    <title>Form</title>
+	</head>
+	
+    <body>
+        <h3>Enter your name</h3>
+
+        <form method="post" action="handler.php">
+            <input type="text" name="username">
+            <input type="submit" value="Send username">
+        </form>
+	</body>
+    
+</html>
